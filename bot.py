2f329a2d06ceaed8de9c2a70a81034d8680a40e3
Cauan Lopes <161593803+CauanFrLopes@users.noreply.github.com>
2026-03-24 20:50:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0'
+    'cookiefile': '/etc/secrets/cookies.txt'
 }
 
 ffmpeg_options = {
